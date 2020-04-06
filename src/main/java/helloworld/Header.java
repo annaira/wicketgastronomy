d8899d14ad82d011842a59058561f444490ed0e7
Anna Ira Hurnaus <anna-ira.hurnaus@procheck24.de>
2020-04-06 12:24:36 +0200

    use bookmarkable link

--- src/main/java/helloworld/Header.java
+++ src/main/java/helloworld/Header.java
@@ -1,38 +1,14 @@
 package helloworld;
 
-import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.panel.Panel;
 
 public class Header extends Panel {
     public Header(String id) {
         super(id);
-        add(new Link<Void>("dashboard") {
-
-            @Override
-            public void onClick() {
-                Header.this.setResponsePage(getApplication().getHomePage());
-            }
-        });
-        add(new Link<Void>("categories") {
-
-            @Override
-            public void onClick() {
-                setResponsePage(CategoriesPage.class);
-            }
-        });
-        add(new Link<Void>("articles") {
-
-            @Override
-            public void onClick() {
-                setResponsePage(ArticlesPage.class);
-            }
-        });
-        add(new Link<Void>("tables") {
-
-            @Override
-            public void onClick() {
-                setResponsePage(TablesPage.class);
-            }
-        });
+        add(new BookmarkablePageLink<Void>("dashboard", getApplication().getHomePage()));
+        add(new BookmarkablePageLink<Void>("categories", CategoriesPage.class));
+        add(new BookmarkablePageLink<Void>("articles", ArticlesPage.class));
+        add(new BookmarkablePageLink<Void>("tables", TablesPage.class));
     }
 }
