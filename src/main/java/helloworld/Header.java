87ad9ac8a86fa33c53238146bae10b048d800858
Anna Ira Hurnaus <anna-ira.hurnaus@procheck24.de>
2020-04-07 15:10:57 +0200

    add PageLink component

--- src/main/java/helloworld/Header.java
+++ src/main/java/helloworld/Header.java
@@ -1,17 +1,21 @@
 package helloworld;
 
-import helloworld.resources.LogoResourceReference;
-import org.apache.wicket.markup.html.image.Image;
-import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.image.ContextImage;
 import org.apache.wicket.markup.html.panel.Panel;
 
 public class Header extends Panel {
+
     public Header(String id) {
         super(id);
-        add(new BookmarkablePageLink<Void>("dashboard", getApplication().getHomePage()));
-        add(new BookmarkablePageLink<Void>("categories", CategoriesPage.class));
-        add(new BookmarkablePageLink<Void>("articles", ArticlesPage.class));
-        add(new BookmarkablePageLink<Void>("tables", TablesPage.class));
-        add(new Image("brand", new LogoResourceReference()));
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+        add(new PageLink("dashboard", getApplication().getHomePage()));
+        add(new PageLink("categories", CategoriesPage.class));
+        add(new PageLink("articles", ArticlesPage.class));
+        add(new PageLink("tables", TablesPage.class));
+        add(new ContextImage("brand", "logo.png"));
     }
 }
---
+++ src/main/java/helloworld/PageLink.java
@@ -0,0 +1,25 @@
+package helloworld;
+
+import org.apache.wicket.Page;
+import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+
+public class PageLink extends BookmarkablePageLink<Void> {
+
+    private final Class<? extends Page> pageClass;
+
+    public PageLink(String id, Class<? extends Page> pageClass) {
+        super(id, pageClass);
+        this.pageClass = pageClass;
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+        final boolean isOwnPageActive = getPage().getClass().equals(this.pageClass);
+        setEnabled(!isOwnPageActive);
+        if (isOwnPageActive) {
+            add(new AttributeAppender("class", " active disabled"));
+        }
+    }
+}
