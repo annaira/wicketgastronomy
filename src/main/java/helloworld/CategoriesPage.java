1d2d42a5f4a28d0b1526485ae986b486e686f15e
Anna Ira Hurnaus <anna-ira.hurnaus@procheck24.de>
2020-04-07 18:08:14 +0200

    redo CategoriesPage with ListView

--- src/main/java/helloworld/CategoriesPage.java
+++ src/main/java/helloworld/CategoriesPage.java
@@ -3,9 +3,16 @@ package helloworld;
 import helloworld.entities.Category;
 import helloworld.services.CategoryService;
 import helloworld.services.ServiceRegistry;
+import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.repeater.RepeatingView;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class CategoriesPage extends BaseEntitiesPage {
     public CategoriesPage(PageParameters parameters) {
@@ -15,10 +22,18 @@ public class CategoriesPage extends BaseEntitiesPage {
     @Override
     protected void onInitialize() {
         super.onInitialize();
-        final RepeatingView categories = new RepeatingView("categories");
-        for (Category category : ServiceRegistry.get(CategoryService.class).listAll()) {
-            categories.add(new Label(categories.newChildId(), category.getName()));
-        }
+
+        final List<Category> categoryList = new ArrayList<>(ServiceRegistry.get(CategoryService.class).listAll());
+
+        final ListView<Category> categories = new ListView<Category>("categories", categoryList) {
+            @Override
+            protected void populateItem(ListItem<Category> listItem) {
+                listItem.add(new Label("name", listItem.getModelObject().getName()));
+
+                final AttributeAppender srcAppender = new AttributeAppender("src", listItem.getModelObject().getImageUrl());
+                listItem.add(new WebMarkupContainer("image").add(srcAppender));
+            }
+        };
         add(categories);
     }
 }
