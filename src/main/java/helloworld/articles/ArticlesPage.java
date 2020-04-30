a50d4f3880e810e01eff5136dbc8b21bd9617838
Anna Ira Hurnaus <anna-ira.hurnaus@procheck24.de>
2020-04-30 12:11:04 +0200

    extract Create Article Form

--- src/main/java/helloworld/articles/ArticlesPage.java
+++ src/main/java/helloworld/articles/ArticlesPage.java
@@ -2,44 +2,21 @@ package helloworld.articles;
 
 import helloworld.BaseEntitiesPage;
 import helloworld.EntityModel;
-import helloworld.SuccessFeedbackPanel;
-import helloworld.ValidationErrorFeedbackPanel;
 import helloworld.services.ArticleService;
-import helloworld.services.ServiceRegistry;
-import org.apache.wicket.bean.validation.PropertyValidator;
 import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.*;
-import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.navigation.paging.IPageable;
 import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.markup.repeater.data.DataView;
 import org.apache.wicket.markup.repeater.data.IDataProvider;
 import org.apache.wicket.model.CompoundPropertyModel;
-import org.apache.wicket.model.Model;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.validation.validator.RangeValidator;
-import org.apache.wicket.validation.validator.UrlValidator;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
 
 public class ArticlesPage extends BaseEntitiesPage {
 
     private final DataView<Article> articles;
-
-    private final Form<Article> form = new Form<Article>("form") {
-
-        @Override
-        protected void onSubmit() {
-            super.onSubmit();
-            form.setVisible(false);
-            ServiceRegistry.get(ArticleService.class).save(form.getModelObject());
-            form.success("Artikel wurde gespeichert");
-        }
-    };
 
     public ArticlesPage(PageParameters parameters) {
         super(parameters);
@@ -71,31 +48,6 @@ public class ArticlesPage extends BaseEntitiesPage {
     protected void onInitialize() {
         super.onInitialize();
         add(this.articles);
-        add(new Link<String>("newArticle") {
-
-            @Override
-            public void onClick() {
-                form.setVisible(true);
-                form.setModelObject(new Article());
-            }
-        });
-        initializeForm();
     }
-
-    private void initializeForm() {
-        form.setVisible(false);
-        add(form);
-        add(new ValidationErrorFeedbackPanel("validationFeedback"));
-        add(new SuccessFeedbackPanel("successFeedback"));
-        form.setModel(new CompoundPropertyModel<>(new EntityModel<>(ArticleService.class)));
-        form.add(new TextField<String>("name").setRequired(true).setLabel(Model.of("Name")));
-        form.add(new DropDownChoice<>("category", new CategoryListModel(), new ChoiceRenderer<>("name", "id")).add(new PropertyValidator<>()));
-        form.add(new TextArea<String>("description").setRequired(true).setLabel(Model.of("Beschreibung")));
-        form.add(new TextField<>("price").setRequired(true).setLabel(Model.of("Preis")).add(new RangeValidator<>(BigDecimal.ZERO, new BigDecimal("20"))));
-        form.add(new TextField<>("validFrom").setLabel(Model.of("Gültig ab")).add(RangeValidator.maximum(LocalDate.now().plusMonths(3))));
-        form.add(new TextField<>("validTo").setLabel(Model.of("Gültig bis")).add(RangeValidator.minimum(LocalDate.now().plusDays(1))));
-        form.add(new TextField<String>("imageUrl").setRequired(true).setLabel(Model.of("Bild-URL")).add(new UrlValidator()));
-    }
-
 
 }
---
+++ src/main/java/helloworld/articles/CreateArticlePage.java
@@ -0,0 +1,55 @@
+package helloworld.articles;
+
+import helloworld.BaseWebPage;
+import helloworld.EntityModel;
+import helloworld.ValidationErrorFeedbackPanel;
+import helloworld.services.ArticleService;
+import helloworld.services.ServiceRegistry;
+import org.apache.wicket.bean.validation.PropertyValidator;
+import org.apache.wicket.markup.html.form.*;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.validation.validator.RangeValidator;
+import org.apache.wicket.validation.validator.UrlValidator;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class CreateArticlePage extends BaseWebPage {
+
+    private final Form<Article> form = new Form<Article>("form") {
+
+        @Override
+        protected void onSubmit() {
+            super.onSubmit();
+            ServiceRegistry.get(ArticleService.class).save(form.getModelObject());
+            setResponsePage(ArticlesPage.class);
+        }
+    };
+
+    public CreateArticlePage(PageParameters parameters) {
+        super(parameters);
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+        initializeForm();
+        form.setModelObject(new Article());
+    }
+
+    private void initializeForm() {
+        add(form);
+        add(new ValidationErrorFeedbackPanel("validationFeedback"));
+        form.setModel(new CompoundPropertyModel<>(new EntityModel<>(ArticleService.class)));
+        form.add(new TextField<String>("name").setRequired(true).setLabel(Model.of("Name")));
+        form.add(new DropDownChoice<>("category", new CategoryListModel(), new ChoiceRenderer<>("name", "id")).add(new PropertyValidator<>()));
+        form.add(new TextArea<String>("description").setRequired(true).setLabel(Model.of("Beschreibung")));
+        form.add(new TextField<>("price").setRequired(true).setLabel(Model.of("Preis")).add(new RangeValidator<>(BigDecimal.ZERO, new BigDecimal("20"))));
+        form.add(new TextField<>("validFrom").setLabel(Model.of("Gültig ab")).add(RangeValidator.maximum(LocalDate.now().plusMonths(3))));
+        form.add(new TextField<>("validTo").setLabel(Model.of("Gültig bis")).add(RangeValidator.minimum(LocalDate.now().plusDays(1))));
+        form.add(new TextField<String>("imageUrl").setRequired(true).setLabel(Model.of("Bild-URL")).add(new UrlValidator()));
+    }
+
+}
