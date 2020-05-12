f77d37eb7bd800ec413e1601eb276bb11beb2f90
Anna Ira Hurnaus <anna-ira.hurnaus@procheck24.de>
2020-05-12 18:44:14 +0200

    editable labels

--- src/main/java/helloworld/articles/EditArticle.java
+++ src/main/java/helloworld/articles/EditArticle.java
@@ -5,8 +5,12 @@ import helloworld.ValidationErrorFeedbackPanel;
 import helloworld.services.ArticleService;
 import helloworld.services.ServiceRegistry;
 import org.apache.wicket.Component;
-import org.apache.wicket.bean.validation.PropertyValidator;
-import org.apache.wicket.markup.html.form.*;
+import org.apache.wicket.extensions.ajax.markup.html.AjaxEditableChoiceLabel;
+import org.apache.wicket.extensions.ajax.markup.html.AjaxEditableLabel;
+import org.apache.wicket.extensions.ajax.markup.html.AjaxEditableMultiLineLabel;
+import org.apache.wicket.markup.html.form.ChoiceRenderer;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.link.ExternalLink;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
@@ -28,12 +32,12 @@ public class EditArticle extends Panel {
             setResponsePage(ArticlesPage.class);
         }
     };
-    private final TextField<String> nameField;
+    private final AjaxEditableLabel<String> nameField;
 
     public EditArticle(String id) {
         super(id);
         this.form.setModel(new CompoundPropertyModel<>(new EntityModel<>(ArticleService.class)));
-        this.nameField = new TextField<>("name");
+        this.nameField = new AjaxEditableLabel<>("name");
     }
 
     @Override
@@ -53,12 +57,12 @@ public class EditArticle extends Panel {
         add(new ValidationErrorFeedbackPanel("validationFeedback"));
 
         form.add(nameField.setRequired(true).setLabel(Model.of("Name")));
-        form.add(new DropDownChoice<>("category", new CategoryListModel(), new ChoiceRenderer<>("name", "id")).setRequired(true).add(new PropertyValidator<>()));
-        form.add(new TextArea<String>("description").setRequired(true).setLabel(Model.of("Beschreibung")));
-        form.add(new TextField<>("price").setRequired(true).setLabel(Model.of("Preis")).add(new RangeValidator<>(BigDecimal.ZERO, new BigDecimal("20"))));
-        form.add(new TextField<>("validFrom").setLabel(Model.of("Gültig ab")).add(RangeValidator.maximum(LocalDate.now().plusMonths(3))));
-        form.add(new TextField<>("validTo").setLabel(Model.of("Gültig bis")).add(RangeValidator.minimum(LocalDate.now().plusDays(1))));
-        form.add(new TextField<String>("imageUrl").setRequired(true).setLabel(Model.of("Bild-URL")).add(new UrlValidator()));
+        form.add(new AjaxEditableChoiceLabel<>("category", null, new CategoryListModel(), new ChoiceRenderer<>("name", "id")));
+        form.add(new AjaxEditableMultiLineLabel<>("description").setRequired(true).setLabel(Model.of("Beschreibung")));
+        form.add(new AjaxEditableLabel<>("price").setRequired(true).setLabel(Model.of("Preis")).add(new RangeValidator<>(BigDecimal.ZERO, new BigDecimal("20"))));
+        form.add(new AjaxEditableLabel<>("validFrom").setLabel(Model.of("Gültig ab")).add(RangeValidator.maximum(LocalDate.now().plusMonths(3))));
+        form.add(new AjaxEditableLabel<>("validTo").setLabel(Model.of("Gültig bis")).add(RangeValidator.minimum(LocalDate.now().plusDays(1))));
+        form.add(new AjaxEditableLabel<String>("imageUrl").setRequired(true).setLabel(Model.of("Bild-URL")).add(new UrlValidator()));
     }
 
     public Component setArticle(Article article) {
